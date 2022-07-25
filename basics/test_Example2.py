9f56653dd396955d229af9fa7a33dce9409228ab
PK <praveennatarajan007@outlook.com>
2022-07-25 11:36:53 +0530

    initial commit

---
+++ basics/test_Example2.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def test_initbrowser():
+    s=Service(ChromeDriverManager().install())
+    driver=webdriver.Chrome(service=s)
+    driver.get("http://www.fb.com")
+    driver.maximize_window()
+    actual_title=driver.title
+    assert actual_title=="Facebook – log in or sign up"
+    driver.close()
